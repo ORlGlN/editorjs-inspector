aecb39a645c81423a24bddf3caa2c3982cd061f6
Tomoyuki Hata <hato6502@gmail.com>
2020-09-18 23:43:46 +0900

    toggle enable/disable

--- src/EditorJSInspector.ts
+++ src/EditorJSInspector.ts
@@ -1,8 +1,16 @@
-import type { InlineTool } from '@editorjs/editorjs';
+import type {
+  API,
+  InlineTool,
+  InlineToolConstructorOptions,
+} from '@editorjs/editorjs';
 
 class EditorJSInspector implements InlineTool {
   static get isInline() {
     return true;
+  }
+
+  static get title() {
+    return 'Inspector';
   }
 
   static domTree({
@@ -64,11 +72,25 @@ class EditorJSInspector implements InlineTool {
     return listItem;
   }
 
+  #api: API;
+  #button: HTMLButtonElement;
   #dialog: HTMLDivElement;
+  #enabled: boolean;
 
-  constructor() {
+  constructor({ api }: InlineToolConstructorOptions) {
+    this.#api = api;
+
+    this.#button = document.createElement('button');
+    this.#button.classList.add(this.#api.styles.inlineToolButton);
+    this.#button.type = 'button';
+    this.#button.innerHTML = `
+      <svg class="icon" height="24" viewBox="0 0 24 24" width="24">
+        <path d="M0 0h24v24H0z" fill="none"/>
+        <path d="M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zM12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z"/>
+      </svg>
+    `;
+
     this.#dialog = document.createElement('div');
-
     this.#dialog.style.position = 'absolute';
     this.#dialog.style.backgroundColor = '#ffffff';
     this.#dialog.style.border = '1px solid #eaeaea';
@@ -78,9 +100,26 @@ class EditorJSInspector implements InlineTool {
     this.#dialog.style.paddingRight = '1rem';
     this.#dialog.style.transform = 'translateY(-100%)';
     this.#dialog.style.zIndex = '1';
+
+    this.#enabled = false;
+  }
+
+  get shortcut() {
+    return 'CMD+SHIFT+I';
   }
 
   checkState() {
+    this.#button.classList.toggle(
+      this.#api.styles.inlineToolButtonActive,
+      this.#enabled
+    );
+
+    if (!this.#enabled) {
+      this.clear();
+
+      return false;
+    }
+
     const selection = window.getSelection();
 
     if (!selection) {
@@ -116,7 +155,7 @@ class EditorJSInspector implements InlineTool {
 
     this.#dialog.append(list);
 
-    return false;
+    return true;
   }
 
   clear() {
@@ -124,16 +163,14 @@ class EditorJSInspector implements InlineTool {
   }
 
   render() {
-    const button = document.createElement('button');
-
-    button.style.display = 'none';
-
     document.body.appendChild(this.#dialog);
 
-    return button;
+    return this.#button;
   }
 
-  surround() {}
+  surround() {
+    this.#enabled = !this.#enabled;
+  }
 }
 
 export default EditorJSInspector;
